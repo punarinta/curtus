05caf3cf9d05f8654751de3fae27dbc1114b667d
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-05-14 13:53:32 +0300

    fall back to default config if smth is missing

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
+const { deepMerge } = require('./utils')
 const config = require('../config.json')
+const distConfig = require('../config.dist.json')
 
 /**
  * @return {{server: {port: number}, inMemory: boolean, redirectOnError: string, salt: number}}
  */
 const getConfig = () => {
-  // TODO: merge with default config
-  return config
+  return deepMerge(distConfig, config)
 }
 
 module.exports = { getConfig }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+/**
+ * @param {import('http').ServerResponse} res
+ */
 const respondToOptions = (res) => {
   res.writeHead(204, {
     'Access-Control-Allow-Origin': '*',
@@ -6,3 +9,50 @@ const respondToOptions = (res) => {
   })
   res.end()
 }
+
+/*
+ * Deep merge two or more objects together.
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ */
+/**
+ * @param {any} objects
+ * @return {Record<string, any>}
+ */
+const deepMerge = (...objects) => {
+  // Setup merged object
+  /**
+   * @type {Record<string, any>}
+   */
+  const newObj = {}
+
+  // Merge the object into the newObj object
+  /**
+   * @param {Record<string, any>} obj
+   */
+  const merge = function (obj) {
+    for (const prop in obj) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (obj.hasOwnProperty(prop)) {
+        // if property is an object, merge properties
+        if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+          // @ts-ignore
+          newObj[prop] = deepMerge(newObj[prop], obj[prop])
+        } else {
+          // @ts-ignore
+          newObj[prop] = obj[prop]
+        }
+      }
+    }
+  }
+
+  // loop through each object and conduct a merge
+  for (let i = 0; i < objects.length; i++) {
+    // @ts-ignore
+    merge(objects[i])
+  }
+
+  // @ts-ignore
+  return newObj
+}
+
+module.exports = { respondToOptions, deepMerge }
